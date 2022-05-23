e3e090ee8aba421f7cc39e82d3420f30494ef46f
Aziah.js <93306237+Primalsia@users.noreply.github.com>
2022-05-23 00:58:03 -0400

    organizing...

---
+++ w1/practice/userdata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter first name: "
+read fname
+echo "Enter last name: "
+read lname
+echo "Enter email: "
+read umail
+echo "Enter username: "
+read uname
+echo "Enter password: "
+read pw
+echo "Whats your fav color? :D"
+read favcolor
+echo "$fname, $lname, $uname, $umail, $pw, $favcolor" >> getdata.csv
